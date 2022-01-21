bb31574a87b028d883425d2adaa21500ec66e798
LF <ladislav.filip@gmail.com>
2022-01-21 21:06:48 +0100

    Inicializace

---
+++ NameDayWorker/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Serilog;
+
+using var log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Seq(serverUrl: "http://localhost:5341")
+    .CreateLogger();
+
+log.Information("Hello, World!");
+
+while (true)
+{
+    Task.Delay(5000);
+    log.Information("Hello, World!");
+}
---
+++ WebNameDays/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace WebNameDays
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InitLogging();
+            
+            Log.Information("Starting web host...");
+            
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        
+        private static void InitLogging()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("SERILOG_SEQ")}.json", true)
+                .Build();
+            
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+    }
+}
