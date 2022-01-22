fc0fc96925c2d353ef4a3bd6e556575a8b5450d0
LF <ladislav.filip@gmail.com>
2022-01-22 10:16:23 +0100

    RESTAPI hledání

---
+++ ApiNameDays/Controllers/DayNamesController.cs
@@ -0,0 +1,72 @@
+﻿#region Info
+
+// FileName:    DayNamesController.cs
+// Author:      Ladislav Filip
+// Created:     22.01.2022
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ApiNameDays.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiNameDays.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DayNamesController : ControllerBase
+    {
+        private readonly ILogger<DayNamesController> _logger;
+
+        public DayNamesController(ILogger<DayNamesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<DayNameService.DayNameItem>), StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            try
+            {
+                var service = GetService();
+                var result = service.DayNames.ToArray();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Unexpected exception");
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{letter}")]
+        [ProducesResponseType(typeof(Dictionary<string, string[]>), StatusCodes.Status200OK)]
+        public IActionResult Get(string letter)
+        {
+            try
+            {
+                var service = GetService();
+                var result = service.Search(letter);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Unexpected exception");
+                return BadRequest();
+            }
+        }
+        
+        private DayNameService GetService()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "..\\Data\\svatky.txt");
+            var service = new DayNameService(filePath, _logger);
+            return service;
+        }
+    }
+}
---
+++ ApiNameDays/Services/DayNameService.cs
@@ -0,0 +1,76 @@
+﻿#region Info
+// FileName:    DayNameService.cs
+// Author:      Ladislav Filip
+// Created:     22.01.2022
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace ApiNameDays.Services
+{
+    public class DayNameService
+    {
+        private readonly string _filePath;
+        private readonly ILogger _logger;
+        private readonly List<DayNameItem> _dayNames = new();
+
+        public IReadOnlyCollection<DayNameItem> DayNames => _dayNames;
+
+        public record DayNameItem(string Day, List<string> Names);
+
+        public DayNameService(string filePath, ILogger logger)
+        {
+            _filePath = filePath;
+            _logger = logger;
+            Init();
+        }
+
+        private void Init()
+        {
+            if (!File.Exists(_filePath))
+            {
+                _logger.LogError("File not found {FilePath}", _filePath);
+                throw new FileNotFoundException();
+            }
+            var lines = File.ReadAllLines(_filePath);
+            Console.WriteLine(lines.Length);
+            Parse(lines);
+        }
+
+        private void Parse(IEnumerable<string> lines)
+        {
+            foreach (var line in lines)
+            {
+                var tmp = line.Split('\t');
+                if (tmp.Length != 2)
+                {
+                    _logger.LogWarning("Unsupported format {Line}", line);
+                    continue;
+                }
+                var day = tmp[0];
+                var names = tmp[1].Split(',').Select(p => p.Trim()).ToList();
+                var item = new DayNameItem(day, names);
+                _dayNames.Add(item);
+            }
+        }
+
+        public Dictionary<string, string[]> Search(string startLetters)
+        {
+            var result = new Dictionary<string, string[]>();
+            foreach (var (day, list) in DayNames)
+            {
+                var names = list.Where(p => p.StartsWith(startLetters, StringComparison.OrdinalIgnoreCase)).ToList();
+                if (names.Any())
+                {
+                    result.Add(day, names.ToArray());
+                }
+            }
+
+            return result;
+        }
+    }
+}
